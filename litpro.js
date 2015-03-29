9ccc91aff2dac8baaa1a5375857a5d46fb68e8a7
James Taylor <jostylr@gmail.com>
2015-03-28 21:43:01 -0400

    v0.8.2
    Bumping it up based on cli

--- litpro.js
+++ litpro.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
 
-/*global process, require, console*/
+/*global process, require */
 
 var mod = require('literate-programming-cli');
 
 var args = mod.opts.parse();
+
+args.build = args.build.map(function (el) {
+    if (el.slice(-1) === "/") {
+        return el.slice(0, -1);
+    } else {
+        return el;
+    }
+});
+
+args.other.forEach(function (arg) {
+    var pair = arg.split(":");
+    if (pair.length === 1) {
+        args[pair[0]] = true;
+    } else if (pair.length === 2) {
+        args[pair[0]] = pair[1]; 
+    } else {
+        args[pair[0]] = pair.slice(0);
+    }
+});
 
 //console.log(args);
 
@@ -13,8 +32,6 @@ var Folder = mod.Folder;
 Folder.prototype.encoding = args.encoding;
 
 Folder.lprc(args.lprc, args);
-
-Folder.cache.firstLoad(args.cache, args.cachefile);
 
 Folder.process(args);
 
